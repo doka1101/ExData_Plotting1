2c4c9df79d879d6c97f1b645e0f684be0fd08292
doka1101 <kangdongsoo@gmail.com>
2014-07-13 19:18:56 -0400

    Submitting Plot 3

---
+++ plot3.R
@@ -0,0 +1,32 @@
+plot3 <- function() {
+    #set working directory
+    setwd("C:/Coursera/Data/")
+    
+    #filename
+    fName <- "household_power_consumption.txt"
+    fPath <- "C:/Coursera/Data Science Track/Exploratory Data Analysis/Project1/"
+    
+    #open file.
+    data <- read.table(fName, header = TRUE, sep = ";", na.strings = "?")
+    
+    #get feb.1 & feb.2 data
+    data.test <- data[with(data, Date=="1/2/2007" | Date == "2/2/2007"), ]
+    dateTime <- strptime( paste(data.test$Date,data.test$Time), 
+                          format="%d/%m/%Y %H:%M:%S")
+    data.test <- cbind(data.test, dateTime)
+    
+    #construct the plot
+    
+    setwd(fPath)
+    png("plot3.png", width = 480, height = 480)
+    with(data.test, plot(dateTime, Sub_metering_1, type="l",col= "black",
+                         ylab = "Global Active Powwer (killowatts)", xlab=""))
+    
+    lines(dateTime, data.test$Sub_metering_2, col = "red")
+    lines(dateTime, data.test$Sub_metering_3, col = "blue")
+    legend("topright", col = c("black", "red", "blue"), lty= 1, 
+           inset = .05, 
+           legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+    
+    dev.off()
+}
